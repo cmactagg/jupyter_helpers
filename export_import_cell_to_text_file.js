f5f649e1d440bc32104bf257224c89cf7a2136a8
cmactagg <mtags22@gmail.com>
2016-10-13 13:32:38 -0600

    Update export_import_cell_to_text_file.js

--- export_import_cell_to_text_file.js
+++ export_import_cell_to_text_file.js
@@ -1,9 +1,16 @@
 %%javascript
+//at the very first line of the cell place the a # followed by name of the cell/file like #import_from_csv
+//when a cell is selected and the g key is pressed, the content of the file will be written out to a python text file
+//with a name of .<cell name>.py
+//the file can then be edited in a regular text editor
+//when done editing, save the file in the text editor
+//then to import it back into jupyter, select the cell, that has the #<cell name> at the top, and press the u key
 
 IPython.keyboard_manager.command_shortcuts.add_shortcut('g', {
     handler : function (event) {
         var input = IPython.notebook.get_selected_cell().get_text();
-        var filename = input.substring(input.lastIndexOf('\n') + 3, input.length);
+        //var filename = input.substring(input.lastIndexOf('\n') + 3, input.length);
+        var filename = input.substring(1, input.indexOf('\n'));
         alert(filename);
         var cmd = "f = open('." + filename + ".py', 'w');f.close()";
         if (input != "") {
@@ -24,7 +31,8 @@ IPython.keyboard_manager.command_shortcuts.add_shortcut('u', {
         }
         var callback = {'output': handle_output};
         var input = IPython.notebook.get_selected_cell().get_text();
-        var filename = input.substring(input.lastIndexOf('\n') + 3, input.length);
+        //var filename = input.substring(input.lastIndexOf('\n') + 3, input.length);
+        var filename = input.substring(1, input.indexOf('\n'));
         var cmd = "f = open('." + filename + ".py', 'r');print(f.read(), end='')";
         IPython.notebook.kernel.execute(cmd, {iopub: callback}, {silent: false});
         return false;
